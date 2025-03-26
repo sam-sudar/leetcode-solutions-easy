0662a3ee1c3350fbf5ade457043700fe5c849f27
iLLiSiO2 <samsudar200@gmail.com>
2025-03-26 20:27:38 +0530

    Palindrome - Complete

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,28 @@
 var isPalindrome = function (x) {
   let reverse = [];
-  console.log(x);
+  // console.log(x);
+  let check;
   arr = x.toString().split("").map(Number);
-  console.log(arr);
+  // console.log(arr);
   for (let i = arr.length - 1; i >= 0; i--) {
     // console.log(reverse);
     reverse.push(arr[i]);
   }
-  console.log(reverse);
-  if (arr === reverse) console.log(true);
+
+  // for (let i = arr.length; i <= 0; i++) {
+  // console.log(reverse);
+  //   if (arr[i] === reverse[i]) {
+  //     check = true;
+  //   }
+  // }
+
+  // console.log(reverse);
+
+  const final = JSON.stringify(arr) === JSON.stringify(reverse);
+  return final;
+
+  // console.log(typeof reverse);
+  // if (arr === reverse) console.log(true);
 };
 
 isPalindrome(121);
